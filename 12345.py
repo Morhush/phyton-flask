601b9c3a52cdfce0acfcbe3f4205939551aa1b45
Morhush <sasha.morhush.z@gmail.com>
2024-06-29 14:10:51 +0300

    create

---
+++ 12345.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template
+import random
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    n = random.randint(0, 1000)
+    k = random.randint(1000, 5000)
+    return render_template('index.html', n=n)
+
+
+@app.route('/login')
+def login():
+    return render_template('index.html')
+
+
+@app.route('/Denis')
+def Denis():
+    return "<h2>O ja suda</h2>"
+
+# Лише для локального сервера (закоментувати)
+if __name__ == '__main__':
+    app.run(debug=True)
--- main.py
+++ main.py
@@ -1,30 +1,36 @@
 from flask import Flask, render_template
-import random
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    n = random.randint(0, 1000)
-    k = random.randint(1000, 5000)
-    return render_template('index.html', n=n)
+    return render_template('index.html')
 
 
 @app.route('/login')
 def login():
-    return render_template('index.html')
+    return render_template('login.html' )
 
 
-@app.route('/Denis')
-def Denis():
-    return "<h2>O ja suda</h2>"
+@app.route('/about')
+def about():
+    return render_template('about.html', title='About')
 
-# Лише для локального сервера (закоментувати)
+
+@app.route('/articles')
+def articles():
+    new_articles = ['How to avoid expensive travel mistakes', 'Top 5 places to experience supernatural forces',
+                    'Three wonderfully bizarre Mexican festivals', 'The 20 greenest destinations on Earth',
+                    'How to survive on a desert island']
+    return render_template('articles.html', articles=new_articles)
+
+
+@app.route('/details')
+def details():
+    return render_template('details.html')
+
+
+# Лише для локаотного сервера (закоментувати)
 if __name__ == '__main__':
     app.run(debug=True)
-
-
-
-
-
