e31087758dacc238511dd196537e25eeb02333bf
Morhush <sasha.morhush.z@gmail.com>
2024-06-29 14:10:51 +0300

    create

--- main.py
+++ main.py
@@ -1,30 +1,34 @@
 from flask import Flask, render_template
-import random
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def index():
-    n = random.randint(0, 1000)
-    k = random.randint(1000, 5000)
-    return render_template('index.html', n=n)
+@app.route('/name')
+def index(name=''):
+    return render_template('index.html')
 
 
 @app.route('/login')
 def login():
-    return render_template('index.html')
+    return render_template('login.html')
 
 
-@app.route('/Denis')
-def Denis():
-    return "<h2>O ja suda</h2>"
+@app.route('/about')
+def about():
+    return render_template('about.html', title='About')
 
-# Лише для локального сервера (закоментувати)
+
+@app.route('/articles')
+def articles():
+    return render_template('articles.html')
+
+
+@app.route('/details')
+def details():
+    return render_template('details.html')
+
+
 if __name__ == '__main__':
     app.run(debug=True)
-
-
-
-
 
